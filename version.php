f5455b65afb081bcb75f35cb6ee0aa209dd0c6e6
Smartlearn-edu <onlymade@gmail.com>
2026-03-14 22:56:48 +0200

    1.0.0

--- version.php
+++ version.php
@@ -5,4 +5,4 @@ $plugin->version   = 2024042800; // Plugin version.
 $plugin->requires  = 2022041900; // Moodle 4.0 or greater.
 $plugin->component = 'local_customhome';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.0';
+$plugin->release   = '1.0.0';

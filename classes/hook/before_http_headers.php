68b864a9092726bd3fe72c49c1caf0ce8e277dac
Smartlearn-edu <onlymade@gmail.com>
2026-03-22 00:45:04 +0200

    v1.0

--- classes/hook/before_http_headers.php
+++ classes/hook/before_http_headers.php
@@ -1,16 +1,38 @@
 <?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
 /**
  * Hooks handlers for local_customhome
  *
  * @package    local_customhome
- * @copyright  2024 Your Name
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @copyright  2025 Mohammad Nabil <mohammad@smartlearn.education>
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 namespace local_customhome\hook;
 
-defined('MOODLE_INTERNAL') || die();
-
+/**
+ * Class before_http_headers
+ *
+ * Handles the redirect hook before HTTP headers are sent.
+ *
+ * @package    local_customhome
+ * @copyright  2024 Smart Learn
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 class before_http_headers {
     /**
      * Hook callback for core\hook\output\before_http_headers
--- prompt.php
+++ prompt.php
@@ -1,10 +1,25 @@
 <?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
 /**
  * AI Prompt Generator Tool
  *
  * @package    local_customhome
- * @copyright  2024 Your Name
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @copyright  2025 Mohammad Nabil <mohammad@smartlearn.education>
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 require_once(__DIR__ . '/../../config.php');
@@ -15,7 +30,18 @@ require_once($CFG->libdir . '/formslib.php');
 admin_externalpage_setup('local_customhome_prompt');
 
 // Define the form.
+/**
+ * Prompt generator form class.
+ *
+ * @package    local_customhome
+ * @copyright  2024 Smart Learn
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 class local_customhome_prompt_form extends moodleform {
+
+    /**
+     * Form definition.
+     */
     public function definition() {
         $mform = $this->_form;
 
